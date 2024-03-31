0809688b25191188b9f350eef1136b1bef9feb07
Daniil <daniil.petro3@gmail.com>
2024-03-31 13:39:52 +0400

    fix: change field in dto

--- internal/server/auth/dtos.go
+++ internal/server/auth/dtos.go
@@ -1,11 +1,11 @@
 package auth
 
 type RegisterRequestDTO struct {
-	Username string `json:"username" validate:"required"`
+	Username string `json:"login" validate:"required"`
 	Password string `json:"password" validate:"required,min=4,max=32"`
 }
 
 type LoginRequestDTO struct {
-	Username string `json:"username" validate:"required"`
+	Username string `json:"login" validate:"required"`
 	Password string `json:"password" validate:"required,min=4,max=32"`
 }
